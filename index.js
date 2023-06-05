a7484a442f8a60fc404d9604989ef29308edf92a
johanesashn <113593955+johanesashn@users.noreply.github.com>
2023-06-05 10:09:51 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 const PORT = process.env.PORT
+const USERNAME = process.env.USERNAME
+const PASSWORD = process.env.PASSWORD
 
 const app = express()
 // in local, other one is using atlas
